65aebc05c4fc0db6ce5201d51b88d3c0ed4d2abc
harshitak4 <137803841+harshitak4@users.noreply.github.com>
2026-01-05 22:52:53 +0530

    Add LeetCode 197 - Rising Temperature

---
+++ Easy/197_Rising_Temperature.sql
@@ -0,0 +1,10 @@
+-- LeetCode 197: Rising Temperature
+-- Difficulty: Easy
+-- Concept: SELF JOIN + DATE comparison
+
+SELECT
+    w2.id
+FROM Weather w1
+JOIN Weather w2
+ON DATEDIFF(w2.recordDate, w1.recordDate) = 1
+WHERE w2.temperature > w1.temperature;
